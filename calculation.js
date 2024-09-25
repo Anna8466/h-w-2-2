1743669516e9b98a6bda8339dee70fed08fa98a1
Anna Tr <anna.trandafil14@gmail.com>
2024-09-25 22:26:46 +0200

    h-w-2-2-3 added

---
+++ calculation.js
@@ -0,0 +1,20 @@
+function increaseBalance(x, y) {
+    return x + y;
+  }
+  
+  function decreaseBalance(x,y) {
+    return x - y;
+  }
+  
+  function divideBalanceByAccounts(balance, accNumber) {
+    return balance/ accNumber;
+  }
+  
+  function getRestAfterDivision(x, y) {
+    return x % y;
+  }
+  
+  console.log(increaseBalance(3000, 700));
+  console.log(decreaseBalance(3000, 700));
+  console.log(divideBalanceByAccounts(3000, 2));
+  console.log(getRestAfterDivision(7000, 3));
